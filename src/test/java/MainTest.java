f3b6f9c0b7e35d321632f1f510c30ab99d5e7311
CaSiOFT <49024619+CaSiOFT@users.noreply.github.com>
2020-11-03 16:00:49 +0800

    Initialize the project.

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void main() {
+        Main.main(null);
+    }
+}
