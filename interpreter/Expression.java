2cabd10a28d9c1416c3a9b90295e10933375d2fc
姜昱九 <54628848+shinenine@users.noreply.github.com>
2020-11-20 18:18:00 +0800

    Interpreter pattern

---
+++ interpreter/Expression.java
@@ -0,0 +1,5 @@
+package interpreter;
+
+public interface Expression {
+    public boolean interpret(String context);
+}
---
+++ interpreter/InterpreterTest.java
@@ -0,0 +1,11 @@
+package interpreter;
+
+public class InterpreterTest {
+    public static void main(String[] args) {
+        Context Team=new Context();
+        Team.result("American hawk");
+        Team.result("Japanese dog");
+        Team.result("Chinese dragon");
+        Team.result("Australian kangaroo");
+    }
+}
