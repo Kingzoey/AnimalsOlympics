e7598a7a6834b57af83611cfa77f7fe8b3fcde21
ma2hua <il>
2020-11-26 03:53:58 +0800

    四个模式
    四个模式

---
+++ src/main/java/com/olympics/Prototype/ProtoTypetrophy.java
@@ -0,0 +1,12 @@
+package com.olympics.Prototype;
+
+public class ProtoTypetrophy {
+    public static void main(String[] args) throws CloneNotSupportedException
+    {
+        trophy obj1=new trophy("猫","选手：在动物运动会中表现优秀，获得击剑项目金牌。","动物运动委员会");
+        obj1.display();
+        trophy obj2=(trophy) obj1.clone();
+        obj2.setName("狗"); 
+        obj2.display();
+    }
+}
