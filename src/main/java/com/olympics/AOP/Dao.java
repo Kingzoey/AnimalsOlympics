26806e78483b1b7777e7f587ccaea3b65073028d
Frezcirno <2277861660@qq.com>
2020-11-25 23:33:13 +0800

    添加AOP模式, 引入了Spring框架的一个包

---
+++ src/main/java/com/olympics/AOP/Dao.java
@@ -0,0 +1,10 @@
+package com.olympics.AOP;
+
+public class Dao {
+    public void business1(){
+        System.out.println("Doing business1.");
+    }
+    public void business2(){
+        System.out.println("Doing business2.");
+    }
+}
