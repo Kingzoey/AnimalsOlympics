8c4acc448b2f7747e7de801a9288c05843e6f6aa
Summer <45913208+Kingzoey@users.noreply.github.com>
2020-11-25 19:21:36 +0800

    Fix memento pattern.

--- src/main/java/com/olympics/Memento/GameMemento.java
+++ src/main/java/com/olympics/Memento/GameMemento.java
@@ -2,18 +2,18 @@ package com.olympics.Memento;
 
 // 备份
 
+import lombok.Getter;
+
 @Getter
 public class GameMemento {
+    private final int playerSteps;
+
     /**
      备份步数 
      * @param playerSteps
      */
     public GameMemento(int playerSteps) {
-        
+        this.playerSteps = playerSteps;
     }
-
-	public int getPlayerStep() {
-		return 0;
-	}
 
 }
