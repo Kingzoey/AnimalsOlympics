362ea03c7eeb6836e5bd5835d4ae1adfe81f0cc2
JYJ <1205748010@qq.com>
2020-11-26 13:05:12 +0800

    增加了测试代码的交互，并在总测试代码中加入了个人负责的模式

--- src/main/java/com/olympics/AbstractFactory/AbstractFactoryTest.java
+++ src/main/java/com/olympics/AbstractFactory/AbstractFactoryTest.java
@@ -1,8 +1,11 @@
 package com.olympics.AbstractFactory;
 
+import java.util.Scanner;
+
 public class AbstractFactoryTest {
     public static void main(String[] args)
     {
+        
         Provider provider_lion=new LionFactory();
         Provider provider_tiger=new TigerFactory();
         MemberProductAbstract pro_lion=provider_lion.produce();
@@ -10,7 +13,14 @@ public class AbstractFactoryTest {
         pro_lion.product();
         pro_tiger.product();
         Provider provider_elephant=new ElephantFactory();
-        MemberProductAbstract pro_elephant=provider_elephant.produce();
-        pro_elephant.product();
+        
+        System.out.println("Do you want to add a new role: elephant? Y/N");
+        Scanner read = new Scanner(System.in);
+        String confirm = read.next();
+        if(confirm.equals("Y"))
+        {
+            MemberProductAbstract pro_elephant=provider_elephant.produce();
+            pro_elephant.product();
+        }
     }
 }
